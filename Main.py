625e982fcc88a1c3feb63f4aefe823aeb2ef0b50
Elena MPC <naviki2@yahoo.es>
2019-10-15 18:14:56 +0200

    initial commit

---
+++ Main.py
@@ -0,0 +1,20 @@
+print("Hey, hola! Vamos a convertir millas en km")
+
+km = int(input("¿Cuántos km quieres pasar a millas? "))
+print(str(km), "km es igual a:", km*0.621371, "millas")
+
+
+while True:
+    Conversion=input("Quieres hacer otra conversión (s/n)?? ")
+
+    if Conversion  == "y":
+        km = int(input("¿Cuántos km quieres pasar a millas? "))
+        print(str(km) + " km son las siguientes millas:")
+        print(km * 0.621371)
+
+    else:
+        print("Vale!, ¡hasta luego!")
+        break
+
+
+
