322b946d5d3dea72d8ddbe05302e0f9e135db56f
Elena MPC <naviki2@yahoo.es>
2019-10-15 22:29:27 +0200

    subo LowerCase

---
+++ LowerCase.py
@@ -0,0 +1,16 @@
+a = 'Today Is A BeautiFul DAY'
+min = a.lower()
+may = a.upper()
+prim = a.capitalize()
+inv = a.swapcase()
+tit = a.title()
+
+print("")
+print("Vamos a corregir esto...:", a)
+
+print("")
+print("Todo minúsculas: ", min)
+print("Todo mayúsculas: ", may)
+print("Primera en mayúsculas: ", prim)
+print("May/mín invertidas: ", inv)
+print("En mayúscula cada palabra: ", tit)
